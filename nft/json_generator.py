7d3798e82f954e545a177715360f226844e02f27
Yoeri De Schepper <yoeri.de.schepper@student.howest.be>
2021-11-24 23:56:11 +0100

    added correct metadata export

--- nft/json_generator.py
+++ nft/json_generator.py
@@ -3,13 +3,16 @@ from PIL import Image
 import os, os.path
 import glob
 path = 'C:/Users/yoeri/PycharmProjects/nft/images/C/'
-
+ipfs_image_path = 'https://gateway.pinata.cloud/ipfs/QmZPc4Jrgq14u4RNzE8BgJg16xGnVYZkZazCx4wL8kCMSs'
+ipfs_metadata_path = 'https://ipfs.io/ipfs/QmWimcL9J1cyJ1nVn5Kb8rfGexT5NNVL2K5ZpfgH74aprG'
 path = "C:/Users/yoeri/PycharmProjects/nft/images/C"
-def generatejson():
+
+
+def generatejson(url,rarity):
     metadata_xx = {
-        "name": "John",
-        "description": 30,
-        "image": "New York",
+        "name": "BLINGathon" + " " + rarity,
+        "description": "NFT minted for participants of the Hackathon at the venue in Brugge - Belgium",
+        "image": ipfs_image_path + "/" + url,
         "properties": "null"
     }
 
@@ -26,6 +29,9 @@ def readfilename():
         for file in files:
 
             file_name = str(os.path.basename(file))
+
+
+
 
 
 
